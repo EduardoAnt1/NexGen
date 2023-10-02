dada43fc482432ed9a77db6c9865e0e1bfb14ca8
PedroF-Alm <pedrofernandesacc@gmail.com>
2023-10-01 22:01:25 -0300

    small changes

--- Nexgen/script.js
+++ Nexgen/script.js
@@ -1,6 +1,7 @@
 function loginForm()
 {
     let login = document.getElementById("login_form")   
+    login.style.pointerEvents = 'all'
     login.style.opacity = '1'    
     
     setTimeout(() => {
@@ -8,9 +9,19 @@ function loginForm()
         {
             if(e.target != login && !login.contains(e.target) && e.target != document.getElementById("sign_in") && e.target != document.getElementById("sign_up"))
             {
-                login.style.opacity = '0'                
+                login.style.opacity = '0'        
+                login.style.pointerEvents = 'none'
                 document.body.onclick = null
             }
         }    
     }, 10)    
+}
+
+function switchForm(code)
+{
+    let log = document.querySelector('#form_log')
+    let cad = document.querySelector('#form_cad')
+
+    log.style.display = code ? '' : 'none'
+    cad.style.display = code ? 'none' : ''
 }
